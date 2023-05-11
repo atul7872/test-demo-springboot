734feac9f3bea548cb6e0cffef918bac8ec637e4
atul7872 <131680578+atul7872@users.noreply.github.com>
2023-05-11 09:52:16 -0400

    demo spring boot application

---
+++ demo/src/main/java/com/example/demo/controller/Student.java
@@ -0,0 +1,20 @@
+package com.example.demo.controller;
+
+public class Student {
+
+	public int getNameid() {
+		return nameid;
+	}
+	public void setNameid(int nameid) {
+		this.nameid = nameid;
+	}
+	public String getFname() {
+		return fname;
+	}
+	public void setFname(String fname) {
+		this.fname = fname;
+	}
+	private int nameid;
+	private String fname;
+	
+}
