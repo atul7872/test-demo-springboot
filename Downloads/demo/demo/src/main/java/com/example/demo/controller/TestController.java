02ecffea5cc13eb2341fe3123f2840a42bfdaf6d
atul <jagtapatulp@yahoo.co.in>
2023-05-11 10:14:28 -0400

    demo-spring

---
+++ Downloads/demo/demo/src/main/java/com/example/demo/controller/TestController.java
@@ -0,0 +1,15 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+	@GetMapping("/")
+	Student  return1() {
+		Student st = new Student();
+		st.setFname("Atul");
+		st.setNameid(2);
+		return st;
+	}
+}
